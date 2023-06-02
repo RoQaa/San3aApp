c758d974a903da17720b35e2e0b8b895b21f58e0
Sohila <SohilaWahed@gmail.com>
2023-06-03 00:03:42 +0300

    handle getAllChat , sendMessage

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,48 +2,60 @@ const Message = require('../models/messageModel')
 const Chat = require('../models/chatModel');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError')
+const uploadImage=require('../utils/uploadImage');
 const {catchAsync} = require('../utils/catchAsync')
 
 exports.sendMessage = catchAsync(async(req, res, next)=>{
-  const {chatId, content} = req.body;
 
-  if(!chatId || !content){
+  if(!req.body.chatId){
     return next(new AppError("Invalid data passed into request"))
-  }  
+  }
 
+  if(!req.body.text){
+    req.body.text = null
+  }
+
+  if(req?.files?.image){
+    const file = req.files.image;
+    req.body.image = await uploadImage(file.tempFilePath);
+  }
+  if (!req?.files?.image) {
+    req.body.image = null;
+  }
+    
   const now = new Date();
-  const sendingtime = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });           
-  console.log(`The Message was sent at ${sendingtime}.`)
-
-  let count = 0;
-
-  const interval = setInterval(() => {
-
-    count++;
-    console.log('textMessage', `This is text message sent from ${count} minute.`)
-
-    if (count >= 2) {
-      clearInterval(interval);
-      console.log('textMessage', `The textMessage was sent at ${sendingtime}.`);
-    }
-  }, 60000); // send message every 60 seconds
-
+  const sendingDate = now.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  const sendingtime = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  
   var newMessage = {
     sender: req.user._id,
-    content: content,
-    chat: chatId,
-    timestamps: Date.now(),
+    text: req.body.text,
+    image: req.body.image,
+    chat: req.body.chatId,
+    time: sendingtime,
+    date: sendingDate,
   };
 
   try{
 
     const message = await Message.create(newMessage)
         
-    await Chat.findByIdAndUpdate(chatId, { latestMessage: message })  
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message })
+    await Chat.findByIdAndUpdate(req.body.chatId, { date: sendingDate })
+    await Chat.findByIdAndUpdate(req.body.chatId, { time: sendingtime })  
     
     res.status(200).json({
         status: true,
         message:"message sent successfully",
+        data:{sender:message.sender,
+          content:{
+            text:message.text,
+            image:message.image
+          },
+          chat:message.chatId,
+          time:message.time,
+          date:message.data
+        }
     })  
     
   }catch(err){
@@ -53,8 +65,10 @@ exports.sendMessage = catchAsync(async(req, res, next)=>{
 
 exports.allMessages = catchAsync(async(req, res, next)=>{
  
-    const messages = await Message.find({ chat: req.body.chatId} , null,{ sort :{timestamp: 1 }})
-                                  .select("-chat")                              
+    const messages = await Message.find({ chat: req.body.chatId})
+                                  .select("-chat")
+                                  .sort({ time: 1 })
+                                  .sort({ date: 1 })                              
 
     if(!messages){
         return next(new AppError(`Sorry, can't found messages belongs to this chat `, 400))

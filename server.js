c6756860b50ee6b42266af699ddb312b222453d5
Sohila <SohilaWahed@gmail.com>
2023-06-14 16:52:58 +0300

    add phone

--- server.js
+++ server.js
@@ -1,63 +1,43 @@
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
- process.on('uncaughtException', err => {
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-   console.log(err.name, err.message);
-   process.exit(1);
- });
-dotenv.config({path:'./config.env'});
-const app=require('./app');
-
-
-const DB=process.env.DATABASE.replace('<password>',process.env.PASSWORD);
-
-
-
-mongoose.connect(DB,{
-  useNewUrlParser:true,
- // useCreateIndex:true,
-  //useFindAndModify:false,
-  useUnifiedTopology: true
-  
-}).then(con=>{
-  // console.log(con);
-   console.log("DB connection Successfully");
+  console.log(err.name, err.message);
+  process.exit(1);
 });
 
-// const tourSchema=new mongoose.Schema({
-//   name:{
-//   type:String,
-//   required: [true,'need name']
-// },
-// rating:{
-//   type:Number,
-//   default:4.4
-// },
-// price:{
-//   type:Number,
-//   required:[true,'must has a price']
-// }
-// });
-// const Tour=mongoose.model('Tour',tourSchema);
-//   const testTour= new Tour({
-//     name:"lost magfier",
-//     price:50
-//   })
-//   testTour.save().then(doc=>{
-//     console.log(doc)
-//   }).catch(err=>{
-//     console.log(err);
-//   })
+dotenv.config({ path: './config.env' });
 
-//console.log(process.env);
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-const port = process.env.PORT||3000;
+io.on('connection', (socket) => {
+  console.log(`New client connected: ${socket.id}`);
+})
+//require('./sockets/chatSocket')(io);
 
-const server = app.listen(port, () => {
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex:true,
+    //useFindAndModify:false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con);
+    console.log('DB connection Successfully');
+  });
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
@@ -65,14 +45,3 @@ process.on('unhandledRejection', err => {
   });
 });
 
-/* ****************SocketIO***************** */
-
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: `http://localhost:${port}`,
-    // credentials: true,
-  },
-});
-
-require("./sockets/chatSocket")(io);

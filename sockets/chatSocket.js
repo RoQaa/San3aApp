06353bb1537d665281c88d1252905be1a3e58b5d
Sohila <SohilaWahed@gmail.com>
2023-06-15 23:37:55 +0300

    add except(socket.id)

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -4,27 +4,27 @@ module.exports = io => {
     console.log("connected to server")
 
     // Join a chat room
-    //const chatRooms = []
-    var clients = []
-    socket.on('join', (userId) => {
-      // socket.join(chatId);
-      // chatRooms[chatId] = chatRooms[chatId] || [];
-      // chatRooms[chatId].push(socket.id);
-      // console.log(chatRooms)
-      console.log(`User ${socket.id} has id  ${userId}`);
-      clients[userId] = socket.id
-      console.log(clients)
+    const chatRooms = []
+    //var clients = []
+    socket.on('join', (chatId) => {
+      socket.join(chatId);
+      chatRooms[chatId] = chatRooms[chatId] || [];
+      chatRooms[chatId].push(socket.id);
+      console.log(chatRooms)
+      console.log(`User ${socket.id} join room ${chatId}`);
+      // clients[userId] = socket.id
+      // console.log(clients)
     });
     
     // Listen for a message from the client
     socket.on('msg', (msg) => {
       // Send the message to all sockets in the room
-      // console.log("chat from client side : " + msg.chat)
-      // console.log("content from client side : " + msg.content)
-      //io.to(msg.chat).emit('res', {content: msg.content, time: msg.time});
-      console.log(msg)
-      let targetId = msg.targetId;
-      if(clients[targetId]) clients[targetId].emit('res', msg)
+      console.log("chat from client side : " + msg.chat)
+      console.log("content from client side : " + msg.content)
+      io.to(msg.chat).except(socket.id).emit('res', {content: msg.content, time: msg.time});
+      // console.log(msg)
+      // let targetId = msg.targetId;
+      // if(clients[targetId]) clients[targetId].emit('res', msg)
     });
       
     socket.on('disconnect', () => {

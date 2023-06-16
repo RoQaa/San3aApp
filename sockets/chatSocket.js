50c6c5828899e9d092ab57f4258da5d02b452172
Sohila <SohilaWahed@gmail.com>
2023-06-16 21:34:13 +0300

    add review and deletePost socket , add data for workers

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -3,28 +3,38 @@ module.exports = io => {
     
     console.log("connected to server")
 
+    //login in APP
+    const clients = []
+    socket.on('login',(user) => {
+      clients[user] = socket.id
+    })
+
+    // review
+    socket.on('review',(users) => {
+      io.to(clients[users.recieveId]).emit('review',`You take review from ${users.senderName}`)
+    })
+
+    //delete post 
+    socket.on('deletePost',(postId)=>{
+      io.to(socket.id).emit('deletePost',postId)
+    })
+
     // Join a chat room
     const chatRooms = []
-    //var clients = []
     socket.on('join', (chatId) => {
       socket.join(chatId);
       chatRooms[chatId] = chatRooms[chatId] || [];
       chatRooms[chatId].push(socket.id);
       console.log(chatRooms)
       console.log(`User ${socket.id} join room ${chatId}`);
-      // clients[userId] = socket.id
-      // console.log(clients)
     });
     
     // Listen for a message from the client
     socket.on('msg', (msg) => {
-      // Send the message to all sockets in the room
       console.log("chat from client side : " + msg.chat)
       console.log("content from client side : " + msg.content)
       io.to(msg.chat).except(socket.id).emit('res', {content: msg.content, time: msg.time});
-      // console.log(msg)
-      // let targetId = msg.targetId;
-      // if(clients[targetId]) clients[targetId].emit('res', msg)
+
     });
       
     socket.on('disconnect', () => {
